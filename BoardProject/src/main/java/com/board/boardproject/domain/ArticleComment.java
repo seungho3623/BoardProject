fc80ac54b1da02c07d34d37b26f9877e9b250cb4
seungho3623 <90664405+seungho3623@users.noreply.github.com>
2023-07-12 01:28:14 +0900

    #8 데이터베이스 접근
    * Article 설계 완료
    * Article Comment 설계 중..

--- BoardProject/src/main/java/com/board/boardproject/domain/ArticleComment.java
+++ BoardProject/src/main/java/com/board/boardproject/domain/ArticleComment.java
@@ -1,11 +1,28 @@
 package com.board.boardproject.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class ArticleComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Article article; //게시글 ID
-    private String content; //내용
+
+    @Setter @ManyToOne(optional = false) private Article article; //게시글 ID
+    @Setter private String content; //내용
 
     private LocalDateTime createAt; //생성 일시
     private String createdBy; //생성자
